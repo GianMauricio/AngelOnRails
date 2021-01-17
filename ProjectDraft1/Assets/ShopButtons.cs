075a810e24a65d35553e597c116f0a4a57dee869
Gian Mauricio <gian_mauricio@dlsu.edu.ph>
2021-01-18 02:02:17 +0800

    Attempt to re-instate shop
    Spoiler: LMOA of course it didn't work

---
+++ ProjectDraft1/Assets/ShopButtons.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Make magic happen with gun upgrades
+/// </summary>
+public class ShopButtons : MonoBehaviour
+{
+
+}
