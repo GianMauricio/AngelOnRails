3c6454321f92b865ea1109d1628b7c431b46b534
JZacharyL <47034052+JZacharyL@users.noreply.github.com>
2021-02-03 18:13:50 +0800

    added advertisement scripts

---
+++ ProjectDraft1/Assets/CustomScripts/AdFinishEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdFinishEventArgs : EventArgs
+{
+    //Args properties
+    public string PlacementID
+    {
+        private set; get;
+    }
+
+    public UnityEngine.Advertisements.ShowResult AdShowResult
+    {
+        private set; get;
+    }
+
+    //Constructor
+    public AdFinishEventArgs(string id, UnityEngine.Advertisements.ShowResult result)
+    {
+        PlacementID = id;
+        AdShowResult = result;
+    }
+}
---
+++ ProjectDraft1/Assets/CustomScripts/AdsManager.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsManager : MonoBehaviour, IUnityAdsListener
+{
+    //ID's of things
+    public const string adInterstitial = "video";
+    public const string adRewarded = "rewardedVideo";
+    public const string adBanner = "sampleBanner";
+
+    //Actual ad event for interstitial and vid ads
+    public event EventHandler<AdFinishEventArgs> OnAdDone;
+
+
+    //Reference to self...?
+    public AdsManager adManager;
+
+    //Reference to the FB manager
+   
+
+    public string GameID
+    {
+        get
+        {
+#if UNITY_ANDROID
+            return "3999029";
+
+#elif UNITY_IOS
+            return "3999028";
+#endif
+            return "check";
+        }
+    }
+
+    public void Start()
+    {
+        //Initialize
+        Advertisement.Initialize(GameID, true);
+
+        Advertisement.AddListener(this);
+        adManager.OnAdDone += AdsManager_OnAdDone;
+    }
+
+    private void AdsManager_OnAdDone(object sender, AdFinishEventArgs e)
+    {
+        //Check if the fully finished
+        if (e.PlacementID == AdsManager.adRewarded)
+        {
+            switch (e.AdShowResult)
+            {
+                case ShowResult.Failed:
+                    Debug.Log("Ad failed to show");
+                    break;
+                case ShowResult.Skipped:
+                    Debug.Log("Betrayal!! ; A;");
+                    break;
+                case ShowResult.Finished:
+                    Debug.Log("Ad completed");
+
+                  
+                    break;
+            }
+        }
+    }
+
+    public void StartBanner()
+    {
+        ShowBannerAd(); /*may not start sometimes, attempt to move this somewhere else...?*/ //TODO:DONE
+    }
+
+    public void ShowBannerAd()
+    {
+        //Try start banner ad
+        StartCoroutine(BannerAdCoRoutine());
+    }
+
+    //Probs will never be used...?
+    public void HideBannerAd()
+    {
+        //If ad is loaded, then hide the ad
+        if (Advertisement.Banner.isLoaded)
+        {
+            Advertisement.Banner.Hide();
+        }
+    }
+
+    //THIS MUST NOT MOVE I DON'T KNOW WHY BUT IT MUST NOT MOVE
+    IEnumerator BannerAdCoRoutine()
+    {
+        while (!Advertisement.isInitialized)
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        //Set position
+        Advertisement.Banner.SetPosition(BannerPosition.TOP_CENTER);
+
+        //Show ad
+        Advertisement.Banner.Show(adBanner);
+    }
+
+    //Implementations of the UnityAdsListener interface
+    public void OnUnityAdsReady(string placementId)
+    {
+        Debug.Log($"Done Loading {placementId}");
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {
+        Debug.Log($"Ads error: {message}");
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+        Debug.Log($"Started Ad {placementId}");
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        //Check if the listener is ready
+        if (OnAdDone != null)
+        {
+            //Make the args!
+            AdFinishEventArgs args = new AdFinishEventArgs(placementId, showResult);
+
+            //Fire!!!!!
+            OnAdDone(this, args);
+        }
+    }
+
+    public void ShowRewardedAd()
+    {
+        //Check if ad is ready
+        if (Advertisement.IsReady(adRewarded))
+        {
+            //Surprise ADS!
+            Advertisement.Show(adRewarded);
+        }
+
+        else
+        {
+            //No ads found
+            Debug.Log("No ads ; A;");
+        }
+    }
+}
