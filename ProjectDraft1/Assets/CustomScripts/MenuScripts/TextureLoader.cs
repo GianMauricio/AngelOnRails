04903b6d6c8d2508f194cc9fd4d84890e41a1f4d
Gian Mauricio <gian_mauricio@dlsu.edu.ph>
2021-02-06 10:03:11 +0800

    Cleaning unused assets

--- ProjectDraft1/Assets/CustomScripts/MenuScripts/TextureLoader.cs
+++ ProjectDraft1/Assets/CustomScripts/MenuScripts/TextureLoader.cs
@@ -15,6 +15,10 @@ public class TextureLoader : MonoBehaviour
     public Material Walls1;
     public Material Floor1;
     public Material FloodLightBase;
+    public Material OilDrum;
+    public Material Ceiling2;
+    public Material Floor2;
+    public Material Walls2;
 
 
     void Start()
@@ -22,51 +26,85 @@ public class TextureLoader : MonoBehaviour
         //Load textures into respective materials
 
         //Room1
-        {
-            //PlainMat
-            Texture albedoPM = assetManager.GetAsset<Texture>("plainmat", "Albedo");
-            Texture normalsPM = assetManager.GetAsset<Texture>("plainmat", "Normal");
-            Texture hmapPM = assetManager.GetAsset<Texture>("plainmat", "HeightMap");
-            Texture ambientoccPM = assetManager.GetAsset<Texture>("plainmat", "AmbOc");
-            Texture dmaskPM = assetManager.GetAsset<Texture>("plainmat", "DetMask");
+        //PlainMat
+        Texture albedoPM = assetManager.GetAsset<Texture>("plainmat", "Albedo");
+        Texture normalsPM = assetManager.GetAsset<Texture>("plainmat", "Normal");
+        Texture hmapPM = assetManager.GetAsset<Texture>("plainmat", "HeightMap");
+        Texture ambientoccPM = assetManager.GetAsset<Texture>("plainmat", "AmbOc");
+        Texture dmaskPM = assetManager.GetAsset<Texture>("plainmat", "DetMask");
 
-            plainmat.SetTexture("_MainTex", albedoPM);
-            plainmat.SetTexture("_BumpMap", normalsPM);
-            plainmat.SetTexture("_ParallaxMap", hmapPM); /*wasted 30 minutes looking for this bugger*/
-            plainmat.SetTexture("_OcclusionMap", ambientoccPM);
-            plainmat.SetTexture("_DetailMask", dmaskPM);
+        plainmat.SetTexture("_MainTex", albedoPM);
+        plainmat.SetTexture("_BumpMap", normalsPM);
+        plainmat.SetTexture("_ParallaxMap", hmapPM); /*wasted 30 minutes looking for this bugger*/
+        plainmat.SetTexture("_OcclusionMap", ambientoccPM);
+        plainmat.SetTexture("_DetailMask", dmaskPM);
 
-            //Walls1
-            Texture albedoW1 = assetManager.GetAsset<Texture>("walls1", "Albedo");
-            Texture normalsW1 = assetManager.GetAsset<Texture>("walls1", "Normal");
-            Texture hmapW1 = assetManager.GetAsset<Texture>("walls1", "HeightMap");
-            Texture ambientoccW1 = assetManager.GetAsset<Texture>("walls1", "AmbOc");
-            Texture emisW1 = assetManager.GetAsset<Texture>("walls1", "Glow");
+        //Walls1
+        Texture albedoW1 = assetManager.GetAsset<Texture>("walls1", "Albedo");
+        Texture normalsW1 = assetManager.GetAsset<Texture>("walls1", "Normal");
+        Texture hmapW1 = assetManager.GetAsset<Texture>("walls1", "HeightMap");
+        Texture ambientoccW1 = assetManager.GetAsset<Texture>("walls1", "AmbOc");
+        Texture emisW1 = assetManager.GetAsset<Texture>("walls1", "Glow");
 
-            Walls1.SetTexture("_MainTex", albedoW1);
-            Walls1.SetTexture("_BumpMap", normalsW1);
-            Walls1.SetTexture("_ParallaxMap", hmapW1);
-            Walls1.SetTexture("_OcclusionMap", ambientoccW1);
-            Walls1.SetTexture("_EmissionMap", emisW1);
+        Walls1.SetTexture("_MainTex", albedoW1);
+        Walls1.SetTexture("_BumpMap", normalsW1);
+        Walls1.SetTexture("_ParallaxMap", hmapW1);
+        Walls1.SetTexture("_OcclusionMap", ambientoccW1);
+        Walls1.SetTexture("_EmissionMap", emisW1);
 
-            //Floor_1
-            /*Why load new bundle when old bundle have correct stuff?*/
-            Texture emisF1 = assetManager.GetAsset<Texture>("floor_1", "Glow");
+        //Floor_1
+        /*Why load new bundle when old bundle have correct stuff?*/
+        Texture emisF1 = assetManager.GetAsset<Texture>("floor_1", "Glow");
 
-            Floor1.SetTexture("_MainTex", albedoPM);
-            Floor1.SetTexture("_BumpMap", normalsPM);
-            Floor1.SetTexture("_ParallaxMap", hmapPM);
-            Floor1.SetTexture("_OcclusionMap", ambientoccPM);
-            Floor1.SetTexture("_DetailMask", dmaskPM);
-            Floor1.SetTexture("_EmissionMap", emisF1);
+        Floor1.SetTexture("_MainTex", albedoPM);
+        Floor1.SetTexture("_BumpMap", normalsPM);
+        Floor1.SetTexture("_ParallaxMap", hmapPM);
+        Floor1.SetTexture("_OcclusionMap", ambientoccPM);
+        Floor1.SetTexture("_DetailMask", dmaskPM);
+        Floor1.SetTexture("_EmissionMap", emisF1);
 
-            //FloodLightbase
-            Texture albedoFLB = assetManager.GetAsset<Texture>("floodlightbase", "Albedo");
-            Texture normalsFLB = assetManager.GetAsset<Texture>("floodlightbase", "Normal");
+        //FloodLightbase
+        Texture albedoFLB = assetManager.GetAsset<Texture>("floodlightbase", "Albedo");
+        Texture normalsFLB = assetManager.GetAsset<Texture>("floodlightbase", "Normal");
 
-            FloodLightBase.SetTexture("_MainTex", albedoFLB);
-            FloodLightBase.SetTexture("_BumpMap", normalsFLB);
-            FloodLightBase.SetTexture("_EmissionMap", emisF1);
-        }
+        FloodLightBase.SetTexture("_MainTex", albedoFLB);
+        FloodLightBase.SetTexture("_BumpMap", normalsFLB);
+        FloodLightBase.SetTexture("_EmissionMap", emisF1);
+        
+
+        //Room2
+        //OilDrum
+        Texture albedo2OD = assetManager.GetAsset<Texture>("oildrum", "Albedo2");
+        Texture normalsOD = assetManager.GetAsset<Texture>("oildrum", "Normal");
+        Texture dmaskOD = assetManager.GetAsset<Texture>("oildrum", "DetMask");
+
+        OilDrum.SetTexture("_DetailAlbedoMap", albedo2OD);
+        OilDrum.SetTexture("_BumpMap", normalsOD);
+        OilDrum.SetTexture("_DetailMask", dmaskOD);
+
+        //Ceiling2
+        Texture ambientoccC2 = assetManager.GetAsset<Texture>("ceiling2", "AmbOc");
+
+        Ceiling2.SetTexture("_OcclusionMap", ambientoccC2);
+        Ceiling2.SetTexture("_BumpMap", normalsPM);
+
+        //Floor2
+        Texture normalsF2 = assetManager.GetAsset<Texture>("floor_2", "Normal");
+        Texture hmapF2 = assetManager.GetAsset<Texture>("floor_2", "HeightMap");
+        Texture ambientoccF2 = assetManager.GetAsset<Texture>("floor_2", "AmbOc");
+
+        Floor2.SetTexture("_BumpMap", normalsF2);
+        Floor2.SetTexture("_ParallaxMap", hmapF2);
+        Floor2.SetTexture("_OcclusionMap", ambientoccF2);
+
+        //Walls2
+        Texture emisW2 = assetManager.GetAsset<Texture>("walls_2", "Glow");
+
+        Walls2.SetTexture("_BumpMap", normalsPM);
+        Walls2.SetTexture("_EmissionMap", emisW2);
+
+
+        //Room3
+
     }
 }
