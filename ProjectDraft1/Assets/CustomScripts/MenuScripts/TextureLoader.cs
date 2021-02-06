7e5530f99d22df3418840b4e388680c6d0e7708f
Gian Mauricio <gian_mauricio@dlsu.edu.ph>
2021-02-06 08:17:17 +0800

    Added one material worth of textures

---
+++ ProjectDraft1/Assets/CustomScripts/MenuScripts/TextureLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Load texture files from bundles and set them to all required materials
+/// </summary>
+public class TextureLoader : MonoBehaviour
+{
+    public string BundlesRootPath
+    {
+        get
+        {
+#if UNITY_EDITOR
+            return Application.streamingAssetsPath;
+#elif UNITY_ANDROID
+return Application.persistentDataPath
+#endif
+        }
+    }
+
+    Dictionary<string, AssetBundle> LoadedBundles = new Dictionary<string, AssetBundle>();
+    public AssetBundle LoadBundle(string bundleName)
+    {
+        
+    }
+}
